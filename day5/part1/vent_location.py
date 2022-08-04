f560c660398318897682a5adca54c9f2e5c0a844
Florian Holl <florian.holl@triaz.de>
2022-08-04 08:38:50 +0200

    Added child class for diagonal lines; rewrote indexing method; added test cases; solved puzzle.

--- day5/part1/vent_location.py
+++ day5/part1/vent_location.py
@@ -14,20 +14,42 @@ class VentLocation:
         self.vector_1, self.vector_2 = locations
         self.x_coordinates = locations[:, 0]
         self.y_coordinates = locations[:, 1]
-        self.difference_vector = self.vector_2 - self.vector_1
+        self.difference_vector = np.abs(self.vector_2 - self.vector_1)
+
+    @property
+    def _is_horizontal_or_vertical_line(self):
+        return 0 in self.difference_vector
 
     def get_vector_of_positions(self) -> Union[np.ndarray, None]:
         """Get the vector of the positions."""
-        if 0 in self.difference_vector:
-            return self._get_final_indices()
+        if self._is_horizontal_or_vertical_line:
+            return self._get_vector()
 
         return None
 
-    def _get_final_indices(self) -> np.ndarray:
-        """Get the final indices of the vents."""
-        self.x_coordinates.sort()
-        self.x_coordinates[1] += 1
-        self.y_coordinates.sort()
-        self.y_coordinates[1] += 1
+    def _get_vector(self) -> np.ndarray:
+        """Get the final vector of the vents."""
+        non_zeros = self.difference_vector[self.difference_vector != 0]
+        difference_length = int(np.unique(non_zeros))
+        x1, x2 = self.x_coordinates
+        y1, y2 = self.y_coordinates
+        x_vector = np.linspace(x1, x2, difference_length + 1).astype(int)
+        y_vector = np.linspace(y1, y2, difference_length + 1).astype(int)
+        vector = np.c_[x_vector, y_vector]
 
-        return np.r_[self.x_coordinates, self.y_coordinates]
+        return vector
+
+
+class AdvancedVentLocation(VentLocation):
+    """A vent location that also allows for diagonal vent positions."""
+
+    @property
+    def _is_diagonal_line(self):
+        return len(np.unique(self.difference_vector)) == 1
+
+    def get_vector_of_positions(self) -> Union[np.ndarray, None]:
+        """Get the vector of positions."""
+        if self._is_horizontal_or_vertical_line or self._is_diagonal_line:
+            return self._get_vector()
+
+        return None
--- day5/part2/test_solution.py
+++ day5/part2/test_solution.py
@@ -1,21 +1,37 @@
 """Test the solution."""
 # pylint: skip-file
 import numpy as np
+import pytest
 
 from day5.part1.read_data import read_day5_data
+from day5.part1.vent_location import AdvancedVentLocation
 from day5.part1.vent_map import VentMap
+
+
+@pytest.mark.parametrize(
+    "vent_locations, expected_result",
+    [
+        (np.array([[0, 0], [7, 7]]), True),
+        (np.array([[0, 2], [5, 7]]), True),
+        (np.array([[9, 7], [7, 9]]), True),
+        (np.array([[9, 7], [5, 3]]), True),
+        (np.array([[7, 9], [4, 6]]), True),
+        (np.array([[7, 9], [6, 5]]), False),
+        (np.array([[7, 9], [6, 1]]), False),
+        (np.array([[0, 5], [5, 5]]), False),
+    ],
+)
+def test_diagonal_lines_are_recognized(vent_locations, expected_result):
+    """Test if diagonal lines are recognized."""
+    result = AdvancedVentLocation(vent_locations)._is_diagonal_line
+    assert result == expected_result
 
 
 def test_game_works_for_example_data():
     """Test if the solution aligns with the example."""
     expected_map = np.array(
         [
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
+            1,
             0,
             1,
             0,
@@ -23,10 +39,26 @@ def test_game_works_for_example_data():
             0,
             0,
             1,
+            1,
+            0,
+            0,
+            1,
+            1,
+            1,
+            0,
+            0,
+            0,
+            2,
             0,
             0,
             0,
             0,
+            2,
+            0,
+            1,
+            0,
+            1,
+            1,
             1,
             0,
             0,
@@ -34,35 +66,52 @@ def test_game_works_for_example_data():
             0,
             1,
             0,
+            2,
             0,
-            0,
-            0,
-            1,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            1,
+            2,
             0,
             0,
             0,
             1,
             1,
             2,
+            3,
             1,
-            1,
-            1,
+            3,
             2,
             1,
             1,
             0,
             0,
             0,
+            1,
+            0,
+            2,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            1,
+            0,
+            0,
+            0,
+            1,
+            0,
+            0,
+            0,
+            0,
+            1,
+            0,
+            0,
+            0,
+            0,
+            0,
+            1,
+            0,
+            0,
+            1,
             0,
             0,
             0,
@@ -70,35 +119,7 @@ def test_game_works_for_example_data():
             0,
             0,
             0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
+            1,
             0,
             2,
             2,
@@ -115,8 +136,8 @@ def test_game_works_for_example_data():
     vent_locations = read_day5_data("day5_test_input.txt")
     vent_map = VentMap(vent_locations)
     for positions in vent_locations:
-        vent_map.update_vent_locations(positions)
+        vent_map.update_vent_locations(positions, allow_diagonals=True)
     assert np.all(vent_map.map.T == expected_map)
 
     result = vent_map.count_nr_of_overlaps()
-    assert result == 5
+    assert result == 12
